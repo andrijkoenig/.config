8074c3343c583ec123c2f837a9a2378315f8a00d
Andrij <47005148+andrijkoenig@users.noreply.github.com>
2025-05-10 21:41:04 +0200

    Updated lsp and telescope setup

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,8 +1,9 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-			{
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        {
             "folke/lazydev.nvim",
             ft = "lua", -- only load on lua files
             opts = {
@@ -12,72 +13,93 @@ return {
                     { path = "${3rd}/luv/library", words = { "vim%.uv" } },
                 },
             },
-			},
         },
-        config = function()
-            local capabilities = require('blink.cmp').get_lsp_capabilities()			
-			
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-			vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})		
-			vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>fr", require("telescope.builtin").lsp_references, {})				  
+        "Hoffs/omnisharp-extended-lsp.nvim",
+        'saghen/blink.cmp',
+		'nvim-telescope/telescope.nvim' ,
+    },
 
-			
-            require'lspconfig'.lua_ls.setup{ 
-				capabilities = capabilities,
-				on_attach = lsp_keymaps,
-			}
-			
-			require'lspconfig'.ts_ls.setup{ 
-				capabilities = capabilities,
-				on_attach = lsp_keymaps,
-			}
-			
-			require("lspconfig")["tinymist"].setup {
+    config = function()
+        local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            automatic_installation = false,
+            ensure_installed = {
+                "lua_ls",
+                "ts_ls",
+                "tinymist",
+                "omnisharp",
+            },
+            handlers = {
+                function(server_name)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+                ["tinymist"] = function()
+                    require("lspconfig")["tinymist"].setup {
                         capabilities = capabilities,
                         settings = {
                             formatterMode = "typstyle",
                             exportPdf = "never"
                         },
                     }
-			-- omnisharp languageserver
-			local pid = vim.fn.getpid()
+                end,
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                runtime = { version = "Lua 5.1" },
+                                diagnostics = {
+                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                                }
+                            }
+                        }
+                    }
+                end,
+                ["ts_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.ts_ls.setup {
+                        capabilities = capabilities,
+                    }
+                end,
+                ["omnisharp"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.omnisharp.setup {
+                        capabilities = capabilities,
+                        enable_roslyn_analysers = true,
+                        enable_import_completion = true,
+                        organize_imports_on_format = true,
+                        enable_decompilation_support = true,
+                        filetypes = { 'cs', 'vb', 'csproj', 'sln', 'slnx', 'props', 'csx', 'targets', 'tproj', 'slngen', 'fproj' },
+                        handlers = {
+                            ["textDocument/definition"] = require('omnisharp_extended').definition_handler,
+                            ["textDocument/typeDefinition"] = require('omnisharp_extended').type_definition_handler,
+                            ["textDocument/references"] = require('omnisharp_extended').references_handler,
+                            ["textDocument/implementation"] = require('omnisharp_extended').implementation_handler,
+                        },
+                    }
+                end,
 
-			require'lspconfig'.omnisharp.setup{ 
-				capabilities = capabilities,
-				on_attach = lsp_keymaps,
-				enable_roslyn_analysers = true,
-				enable_import_completion = true,
-				organize_imports_on_format = true,
-				enable_decompilation_support = true,
-				filetypes = { 'cs', 'vb', 'csproj', 'sln', 'slnx', 'props', 'csx', 'targets', 'tproj', 'slngen', 'fproj' }, 
-				cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(pid) },
-			}
-        end,
-    },  {
-    "williamboman/mason.nvim",
-    config = function()
-		require("mason").setup({
-			PATH = "prepend", -- "skip" seems to cause the spawning error
-		})
-	end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup {
-		ensure_installed = {
-                "lua_ls",
-                "ts_ls",
-                "tinymist",				
-                "omnisharp",
-            },
-	  }
-    end,
-  },
+            }
+
+        })
+
+        vim.api.nvim_create_autocmd('LspAttach', {		
+            callback = function(e)						
+				local builtin = require('telescope.builtin')
+				
+				vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, { desc = "Go to Definition", buffer = e.buf })
+				vim.keymap.set("n", "gi", function() builtin.lsp_implementations() end, { desc = "Go to Implementation ", buffer = e.buf })
+				vim.keymap.set("n", "gr", function() builtin.lsp_references() end, { desc = "Show References", buffer = e.buf })
+                vim.keymap.set("n", "K",  function() vim.lsp.buf.hover() end, { buffer = e.buf, desc = "Hover Documentation" })
+                vim.keymap.set("n", "<leader>cf", function() vim.lsp.buf.format() end, { buffer = e.buf, desc = "Format Document" })
+                vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, { buffer = e.buf, desc = "Code Actions" })
+                vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.rename() end, { buffer = e.buf, desc = "Rename Symbol" })
+            end
+        })
+    end
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,9 +2,20 @@ return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
   dependencies = { 
     'nvim-lua/plenary.nvim',
-    'nvim-treesitter/nvim-treesitter'
+    'nvim-treesitter/nvim-treesitter',
+	'nvim-telescope/telescope-ui-select.nvim'
   },
   config = function()
+  -- This is your opts table
+	require("telescope").setup {
+	  extensions = {
+		["ui-select"] = {
+		  require("telescope.themes").get_dropdown {}
+		}
+	  }
+	}
+	require("telescope").load_extension("ui-select")  
+  
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
