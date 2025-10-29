9f13fc2047b5609a3c3973329edb776269429881
Andrij König <andrij.koenig@gmail.com>
2025-10-29 16:39:41 +0100

    nvim treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
                 "vimdoc", "javascript", "typescript", "c", "c_sharp", "lua", "cpp", "cmake",
                 "java", "bash", "typst",
             },
-            sync_install = false,
+            sync_install = true,
             auto_install = false,
             highlight = {
                 enable = true
