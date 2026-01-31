04af21615dad505890e007788e55fa7b784e7ff2
Andrij König <47005148+andikon@users.noreply.github.com>
2026-01-31 10:47:53 +0100

    fix: update DotfileSync path which caused a bug

--- DotfileSync.ps1
+++ DotfileSync.ps1
@@ -1,7 +1,7 @@
 $Root = Split-Path -Parent $MyInvocation.MyCommand.Path
-$Src  = Join-Path $Root DotfileSync.javaDotfileSync.java
-$Out  = Join-Path $Root .dotfiles-sync
+$Src  = Join-Path $Root "DotfileSync.java\DotfileSync.java"
+$Out  = Join-Path $Root ".dotfiles-sync"
 
-New-Item -ItemType Directory -Force -Path $Out  Out-Null
+New-Item -ItemType Directory -Force -Path $Out | Out-Null
 javac -d $Out $Src
 java -cp $Out DotfileSync @args
